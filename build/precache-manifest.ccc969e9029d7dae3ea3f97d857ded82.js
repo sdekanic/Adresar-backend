d380e831e07bf334be67ada0f35f3f530dfc6723
Sanela Dekanić <sdekanic@pmfst.hr>
2020-10-28 01:06:24 +0100

    Backend

---
+++ build/precache-manifest.ccc969e9029d7dae3ea3f97d857ded82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645ffcba22045cb4c69cc38cb045eb9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04ba14d2c31a103be402",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a02e3a831fc74fe41876",
+    "url": "/static/js/2.7c356cc9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7c356cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ba14d2c31a103be402",
+    "url": "/static/js/main.bc1e0e3e.chunk.js"
+  },
+  {
+    "revision": "964f16665a4adf2f7d54",
+    "url": "/static/js/runtime-main.bb9fe5b6.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+
+const zahtjevInfo = (req, res, next) => {
+    console.log('Metoda:', req.method)
+    console.log('Putanja:', req.path)
+    console.log('Tijelo:', req.body)
+    console.log('---')
+    next()
+  }
+  
+  app.use(zahtjevInfo)
+
+let podaci = [
+    {
+        id: 1,
+        ImePrezime: 'Sanela Dekanić',
+        Email: 'sdekanic@pmfst.hr',
+        vazno: true
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Pozdrav od Express servera + nodemona</h1>')
+})
+
+app.get('/api/poruke', (req, res) => {
+    res.json(podaci)
+})
+
+app.get('/api/poruke/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const osoba = podaci.find(p => p.id === id)
+
+    if (osoba) {
+        res.json(osoba)
+    } else {
+        res.status(404).end()
+    }
+
+})
+app.delete('/api/poruke/:id', (req, res) => {
+    const id = Number(req.params.id)
+    podaci = podaci.filter(p => p.id !== id)
+    res.status(204).end()
+
+})
+
+app.put('/api/poruke/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const podatak = req.body
+    podaci = podaci.map(p => p.id !== id ? p : podatak)
+    res.json(podatak)
+
+})
+
+app.post('/api/poruke', (req, res) => {
+    const maxId = podaci.length > 0
+    ? Math.max(...podaci.map(p => p.id))
+    : 0
+
+    const podatak = req.body
+    if(!podatak.ImePrezime || !podatak.Email){
+        return res.status(400).json({
+            error: 'Nedostaje sadržaj'
+        })
+    }
+    const osoba = {
+        ImePrezime: podatak.ImePrezime,
+        Email: podatak.Email,
+        vazno: podatak.vazno || false,
+        id: maxId + 1
+    }
+
+    podaci = podaci.concat(osoba) 
+    res.json(osoba)
+})
+
+const nepoznataRuta = (req, res) => {
+    res.status(404).send({ error: 'nepostojeca ruta' })
+  }
+  
+  app.use(nepoznataRuta)
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server sluša na portu ${PORT}`);
+})
